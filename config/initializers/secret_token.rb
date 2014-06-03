4cc123c13f8e8e15e46b8a1dddc6e501cfa6ae34
VaradJoshi-Pivotal <vjoshi@pdx2-office-dhcp24.eng.vmware.com>
2014-06-02 17:12:03 -0700

    First set of changes to connect to GFXD from ruby.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gemfirexdtest::Application.config.secret_token = '8dfc45c5a417fd07d49dd0ce6d3eb69cdacd39719e1856d4eb04dabecf5b1bc3d20b20af11f7f9827988caea547b0f93463f09e1bf14766ded49ae807ecfbbaf'
